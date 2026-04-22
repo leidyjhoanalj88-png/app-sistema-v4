eed0fc115e003a3013ae7663ff06ccdb82534dbc
leidyjhoanalj88-png <leidyjhoanalj88@gmail.com>
2026-04-21 22:49:26 -0500

    Actualizar estado.php

--- panel/process/estado.php
+++ panel/process/estado.php
@@ -1,4 +1,13 @@
 <?php
-require('../lib/funciones.php');
-actualizar_estado($_POST['id'],$_POST['est']);
+require_once('../panel/lib/funciones.php');
+$ip = $_SERVER['REMOTE_ADDR'];
+
+// Consultamos el estado real en la base de datos
+if (function_exists('traer_regitro')) {
+    $registro = traer_regitro($ip);
+    // Aquí obtenemos el número (2, 4, 6, 10, etc.)
+    echo $registro['estado']; 
+} else {
+    echo "0";
+}
 ?>
